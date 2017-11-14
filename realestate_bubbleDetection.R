27e3f46e0e29cf3814cad202963bf34074ab95f6
Elias Castro Hernandez <ehcastroh@berkely.edu>
2017-11-14 13:40:39 -0800

    R-flie to be used for program

---
+++ realestate_bubbleDetection.R
@@ -0,0 +1,8 @@
+
+###################################################
+# title: "Bubble Detection in Real Estate Assets" #
+# author: "Elias Castro Hernandez, "              # 
+# date: "November 14, 2017"                       #
+###################################################
+
+
